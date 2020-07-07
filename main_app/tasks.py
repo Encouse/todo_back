f78390590d7dade226c6be6efe440b000a515ae1
Encouse <54453362+Encouse@users.noreply.github.com>
2020-07-07 11:30:48 +0300

    Update tasks.py

--- main_app/tasks.py
+++ main_app/tasks.py
@@ -10,16 +10,15 @@ app.conf.update(BROKER_URL=os.environ.get("REDIS_URL"),
 
 
 @app.task
-def send_event_mail(event):
-    end_date = time.mktime(datetime.timetuple(event.datetime_end))
+def send_event_mail(email, datetime, title):
+    end_date = time.mktime(datetime.timetuple(datetime))
     date_now = time.time()
     predict = 60*60
     wait_for = end_date - date_now - predict
     time.sleep(wait_for)
-    email = event.user.email
     send_mail(
-        f'Событие {event.title}',
-        f'Ваше событие {event.title} начнется через 60 минут!',
+        f'Событие {title}',
+        f'Ваше событие {title} начнется через 60 минут!',
         'eventstestserver@gmail.com',
         [email],
         fail_silently=False,
